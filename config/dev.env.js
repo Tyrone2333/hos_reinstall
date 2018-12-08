791ccf75e683976634df9d902b74cef3284631ba
en20 <424021424@qq.com>
2018-12-08 11:05:45 +0800

    更改 gitignore

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,8 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
     NODE_ENV: '"development"',
+    HOST: `'${getIPAddress()}'`,
+    PORT: '10088',
     // BASE_API: '"http://192.168.1.142:80"', // 本机php
     // BASE_API: '"http://127.0.0.1:80"',
     // BASE_API: '"http://192.168.1.142:10010/hos_api"',// 本机node 宿舍
